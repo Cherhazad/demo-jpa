b879a9663246ca8e6666a933dc2002e37c67f081
Cherhazad <cherhazad.boustani@outlook.fr>
2024-06-03 12:34:33 +0200

    Correction TP1 - boucle pour afficher la liste de données

--- src/main/java/fr/diginamic/ConnexionJpa.java
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -1,9 +1,12 @@
 package fr.diginamic;
 
+import java.util.List;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 public class ConnexionJpa {
 
@@ -20,11 +23,20 @@ public class ConnexionJpa {
 		
 		//em.persist(region1); 
 		
-		// extraire une région
+		// extraire une région unique
 		
 		region1 = em.find(Region.class, 2);
 		if (region1 != null) {
 			System.out.println(region1);
+		}
+		
+		//extraire une liste
+		
+		TypedQuery<Region> query = em.createQuery("select r from Region r", Region.class); // r est un alias comme en SQL, ici c'est du JPQL qui fonctionne en orienté objet et non SQL, le * n'est pas accepté, on le remplace par un alias créé arbitrairement
+		List<Region> listesRegion =query.getResultList();
+		
+		for (Region r: listesRegion) {
+			System.out.println(r);
 		}
 		
 		transaction.commit();
